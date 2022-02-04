954ab938c09b9196f54ea4efec6b29d0beb44e16
Heitor Garcia de Souza <heitorsome@gmail.com>
2022-02-04 16:34:06 -0300

    feat: added count itens in HeaderCart

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
-import CartProvider from "./components/store/CartProvider";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartShow, setCartShow] = useState(false);
