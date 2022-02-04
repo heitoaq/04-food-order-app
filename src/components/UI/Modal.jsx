c694abc9a317a0e7489e1d804d1dc99da9a6fbfe
Heitor Garcia de Souza <heitorsome@gmail.com>
2022-02-04 15:07:02 -0300

    fix: adjustment to open and close Modal

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
 
 const BackDrop = (props) => {
-  return <div className={classes.backdrop} />;
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 
 const ModalOverlay = (props) => {
@@ -21,7 +21,10 @@ const Modal = (props) => {
     <Fragment>
       {/* <BackDrop />
       <ModalOverlay>{props.children}</ModalOverlay> */}
-      {ReactDOM.createPortal(<BackDrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <BackDrop onClose={props.onClose} />,
+        portalElement
+      )}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
