5a56d385b177d0838af87862f020d546674166b5
Heitor Garcia de Souza <heitorsome@gmail.com>
2022-02-09 17:16:28 -0300

    feat: now the items are shown correctly in the cart

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -12,11 +12,7 @@ const MealItemForm = (props) => {
     const enteredAmount = amountInputRef.current.value;
     const enteredAmountNumber = +enteredAmount;
 
-    if (
-      enteredAmount.trim.length === 0 ||
-      enteredAmountNumber < 1 ||
-      enteredAmountNumber > 5
-    ) {
+    if (enteredAmountNumber < 1 || enteredAmountNumber > 5) {
       setAmountIsValid(false);
       return;
     }
